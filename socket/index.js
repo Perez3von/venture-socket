f343f4c9fbf8ae455f8158d103a277c8daf3e826
Evon Perez <perez.evon@gmail.com>
2022-03-15 07:48:57 -0400

    working socket

---
+++ socket/index.js
@@ -0,0 +1,35 @@
+const io = require('socket.io')(8900, {
+    cors:{
+        origin: 'http://localhost:3000'
+    }
+});
+
+
+
+let users = [];
+//track users? rooms?
+// <-- this is the problem
+io.on('connection', (socket) => {
+    let currRoom
+
+    console.log('connected', socket.id);
+
+    socket.on('create', ({room})=>{
+        currRoom = room;
+        socket.join(room);
+        console.log(room);
+    });
+
+    socket.on('sendMessage', ({ msg }) =>{
+
+        io.to(currRoom).emit('getMessage', {
+            myMessage:msg
+        })
+        console.log(msg);
+
+    })
+
+    socket.on('disconnect', ()=>{
+        console.log('disconnected', socket.id)
+    })
+});
